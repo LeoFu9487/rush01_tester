5579b7ffcbcf95029c0d2b7dfd4fde684f6574a9
LeoFu9487 <leofu890806@gmail.com>
2021-04-12 14:36:17 +0200

    test.sh : time check

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
+TIME_LIMIT=1
 for i in {1..10}
 do
 	[[ -f ./our_ans/ans$i ]] || cat ./map/map$i | ./our_ans/our_rush > ./our_ans/ans$i
@@ -13,25 +14,31 @@ else
 	echo "rush not found, please make sure you have the executable file ../rush"
 	exit 1
 fi
-start=`date +%s%N`
-for i in {1..10}
-do
-	cat map/map$i | ../rush > your_ans/ans$i
-	diff your_ans/ans$i our_ans/ans$i > diff/diff$i.txt
-done
 flag="${GREEN}OK${NC}\n"
 for i in {1..10}
 do
+	start=`date +%s%N`
+	cat map/map$i | timeout $TIME_LIMIT ../rush > your_ans/ans$i
+	time_check=$(echo $?)
+	diff your_ans/ans$i our_ans/ans$i > diff/diff$i.txt
+	printf "test $i result : "
 	if [[ -s diff/diff$i.txt ]];
 	then
 		flag="${RED}KO${NC}\n"
+		if [[ "$time_check" = "124" ]]
+		then
+			printf "${RED}TLE${NC}\n"
+		else
+			printf "${RED}KO${NC}\n"
+		fi
 	else
 		rm -rf diff/diff$i.txt
+		printf "${GREEN}OK${NC}\n"
+		end=`date +%s%N`
+		run_time=`expr $end - $start`;
+		base_time=1000000
+		printf "execution time : "
+		printf "$((run_time / base_time)) ms\n"
 	fi
 done
 printf "test result : $flag"
-end=`date +%s%N`
-run_time=`expr $end - $start`;
-base_time=1000000
-printf "execution time : "
-printf "$((run_time / base_time)) ms\n"
